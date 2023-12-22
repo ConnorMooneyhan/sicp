1b07d6882a2ad3b7aad4d308edd8eac9a51599a5
Connor Mooneyhan <connmoo@gmail.com>
2023-12-21 20:32:12 -0500

    Get iterative on that sum (ex. 1.30)

---
+++ sum.scm
@@ -0,0 +1,11 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+(define (better-sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
